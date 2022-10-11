35a04528c2c38770ba66fa7573a8b52937ec4bee
Huang Daoxu <34485325+sanjayheaven@users.noreply.github.com>
2022-10-11 21:53:30 +0800

    fix: add Bearer token standard

--- src/api/axios/index.ts
+++ src/api/axios/index.ts
@@ -1,46 +1,46 @@
-import axios from "axios"
-import config from "../../config/env"
-import { message } from "antd"
+import axios from "axios";
+import config from "../../config/env";
+import { message } from "antd";
 
 const instance = axios.create({
   baseURL: config.instance.baseURL,
   timeout: 10000,
   // headers: { "Content-Type": "application/json" },
-})
+});
 
 instance.interceptors.request.use(
   function (config) {
-    const token = localStorage.authToken
+    const token = localStorage.authToken;
     if (token) {
-      config.headers["authorization"] = localStorage.authToken
+      config.headers["Authorization"] = `Bearer ${localStorage.authToken}`;
     }
-    return config
+    return config;
   },
   function (error) {
-    return Promise.reject(error)
+    return Promise.reject(error);
   }
-)
+);
 
 instance.interceptors.response.use(
   (response) => {
-    const { method } = response.config
+    const { method } = response.config;
     if (["post", "put", "delete", "patch"].includes(method)) {
-      message.success("Success!")
+      message.success("Success!");
     }
-    return response.data
+    return response.data;
   },
   function (error) {
-    const info: { code?: number; message?: string } = {}
+    const info: { code?: number; message?: string } = {};
     if (!error.response) {
-      info.code = 500
-      info.message = "Network Error"
+      info.code = 500;
+      info.message = "Network Error";
     } else {
-      info.code = error.response.data?.code || 500
-      info.message = error.response.data?.message || "Internal Server Error"
+      info.code = error.response.data?.code || 500;
+      info.message = error.response.data?.message || "Internal Server Error";
     }
-    message.destroy()
-    message.error(info.message)
-    return Promise.reject(info)
+    message.destroy();
+    message.error(info.message);
+    return Promise.reject(info);
   }
-)
-export default instance
+);
+export default instance;
