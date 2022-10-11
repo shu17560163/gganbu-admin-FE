34984951997f77e39f01413ef9d2dc31bc206204
Huang Daoxu <34485325+sanjayheaven@users.noreply.github.com>
2022-10-11 23:46:26 +0800

    fix: update build to vite build

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: { port: 9527 },
   plugins: [react()],
+
+  // drop console
   build: {
+    minify: "terser",
     terserOptions: {
       compress: {
         drop_console: true,
@@ -13,4 +16,4 @@ export default defineConfig({
       },
     },
   },
-})
+});
