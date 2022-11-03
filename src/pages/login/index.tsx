aec181e448367426fdb9ff3e1d2200ffefa23218
Huang Daoxu <34485325+sanjayheaven@users.noreply.github.com>
2022-11-04 02:00:42 +0800

    refactor: login page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,10 +1,23 @@
-import { Tabs, Input, Button, message, notification } from "antd"
+import { Tabs, Input, Button, message, notification, Checkbox, Divider } from "antd"
 
-import { User, Lock, EnvelopeSimple, DeviceMobileSpeaker } from "phosphor-react"
+import {
+  User,
+  Lock,
+  EnvelopeSimple,
+  DeviceMobileSpeaker,
+  TwitterLogo,
+  FacebookLogo,
+  GoogleLogo,
+  GithubLogo,
+  Eye,
+  EyeSlash,
+} from "phosphor-react"
 
 import { useState } from "react"
 import { useNavigate } from "react-router"
 import { StaffApi } from "../../api"
+import { createFormItems } from "../../components/form/formConfig"
+import { useThemeContext } from "../../context"
 
 export function timeFix() {
   const hour = new Date().getHours()
@@ -15,6 +28,7 @@ const Login = () => {
   const navigate = useNavigate()
   const [username, setUsername] = useState("admin")
   const [password, setPassword] = useState("admin")
+  const [showPassword, setShowPassword] = useState(false)
   const [phone, setPhone] = useState("")
   const [code, setCode] = useState("")
 
@@ -24,6 +38,12 @@ const Login = () => {
     smsSendBtn: false, // 转圈圈
     time: 60, // 倒计时60秒
   })
+
+  const { theme } = useThemeContext()
+  const { primaryColor } = theme
+
+  const iconSize = 26
+  const thirdParty = [{ icon: GoogleLogo }, { icon: TwitterLogo }, { icon: FacebookLogo }, { icon: GithubLogo }]
 
   const getCode = async () => {
     if (!/^\d{8}$/.test(phone)) {
@@ -86,28 +106,103 @@ const Login = () => {
   return (
     // userlayout
     <div
-      className="w-full h-full relative"
+      className="w-full h-full flex items-center justify-center"
       style={{
         background: "linear-gradient(180deg, #a1dff5 0%, #e4eff8 100%)",
       }}
     >
-      {/* <img
-        className="absolute left-0 right-0 top-0 bottom-0 lg:h-screen m-auto opacity-70 max-w-full"
-        src={Logo}
-        alt=""
-      /> */}
-      {/* loginFormWrapper  */}
-      <div
-        className="w-96 absolute left-0 right-0 top-0 bottom-0 m-auto flex items-center h-max"
-        style={{ minWidth: 260 }}
-      >
-        <div>
-          {/* header text */}
-          <h1 className="text-center text-5xl">Gganbu Admin</h1>
+      {/* login content */}
+      <div className="m-3 md:m-0 w-full md:w-max p-10 px-4 sm:px-10 rounded-2xl bg-white">
+        {createFormItems([
+          {
+            children: <h1 className=" text-center text-3xl">Gganbu Admin</h1>,
+          },
+          {
+            required: true,
+            children: (
+              <Input
+                className=" md:w-[350px]"
+                allowClear
+                size="large"
+                placeholder="Username"
+                prefix={<User size={20} />}
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                onPressEnter={() => handleLogin()}
+              />
+            ),
+          },
+          {
+            required: true,
+            children: (
+              <Input
+                className=" md:w-[350px]"
+                size="large"
+                type={!showPassword && "password"}
+                placeholder="Password"
+                prefix={<Lock size={20} />}
+                suffix={
+                  (showPassword && (
+                    <Eye className=" cursor-pointer" onClick={() => setShowPassword(!showPassword)} size={20} />
+                  )) || (
+                    <EyeSlash className=" cursor-pointer" onClick={() => setShowPassword(!showPassword)} size={20} />
+                  )
+                }
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                onPressEnter={() => handleLogin()}
+              />
+            ),
+          },
+          {
+            children: (
+              <>
+                <div className="flex items-center justify-between" style={{ color: primaryColor }}>
+                  <Checkbox style={{ color: primaryColor }}>
+                    {/* 自动登录 */}
+                    Remember Me
+                  </Checkbox>
+                  <div className=" cursor-not-allowed">
+                    {/* 忘记密码 */}
+                    Forgot Password
+                  </div>
+                </div>
+              </>
+            ),
+          },
+          {
+            children: (
+              <Button block loading={loading} size="large" type="primary" onClick={() => handleLogin()}>
+                Sign In
+              </Button>
+            ),
+          },
+          {
+            children: (
+              <>
+                <Divider style={{ margin: 0 }}>or</Divider>
+              </>
+            ),
+          },
+          {
+            className: "mb-0",
+            children: (
+              <div className=" flex justify-between">
+                {thirdParty.map((item) => {
+                  return (
+                    <>
+                      <div className=" bg-gray-200 rounded p-2 flex items-center cursor-pointer cursor-not-allowed">
+                        <item.icon size={iconSize} weight="fill" color={primaryColor} />
+                      </div>
+                    </>
+                  )
+                })}
+              </div>
+            ),
+          },
+        ])}
 
-          {/* login form  */}
-          <div className="py-16 px-5 rounded-2xl m-3" style={{ backgroundColor: "rgba(0, 0, 0, 0.1)" }}>
-            <Tabs type="card" centered onChange={(key) => setTabKey(key)}>
+        {/* <Tabs type="card" centered onChange={(key) => setTabKey(key)}>
               <Tabs.TabPane key="username" tab="Username">
                 <Input
                   className="mb-4"
@@ -125,6 +220,7 @@ const Login = () => {
                   type="password"
                   placeholder="Password"
                   prefix={<Lock size={20} />}
+                  suffix={<Lock size={20} />}
                   className="mb-4"
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
@@ -157,30 +253,19 @@ const Login = () => {
                   </Button>
                 </div>
               </Tabs.TabPane>
-            </Tabs>
-            <Button
-              loading={loading}
-              className="w-full h-10 text-lg py-0 px-4"
-              size="large"
-              type="primary"
-              onClick={() => handleLogin()}
-            >
-              Log in
-            </Button>
-          </div>
-        </div>
+            </Tabs> */}
       </div>
 
       {/* footer */}
 
-      <div className="fixed bottom-0 w-full p-10">
+      {/* <div className="fixed bottom-0 w-full p-10">
         <div className="mb-2 flex justify-center items-center">
           <a className="mr-10">Help</a>
           <a className="mr-10">Policy</a>
           <a>Service</a>
         </div>
         <div className=" flex justify-center">Copyright &copy; 2022</div>
-      </div>
+      </div> */}
     </div>
   )
 }
