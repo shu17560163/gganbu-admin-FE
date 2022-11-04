c8ac4e68f88074a198ec1828f177ae9e8ce33380
Huang Daoxu <34485325+sanjayheaven@users.noreply.github.com>
2022-11-04 20:46:22 +0800

    fix: modify the base url when deploy to gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/", // default '/', need to modify when deploy to GitHub Pages
   server: { port: 9527 },
   plugins: [react()],
 
